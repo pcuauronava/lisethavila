cc9e3673fb0de042fb617f3af5bd17a39729d433
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2025-11-12 01:26:25 -0500

    added qr code

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RAFMBLJY.mjs";import"./chunk-EH4RH2P2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-CA47KVD6.mjs";import"./chunk-EH4RH2P2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
